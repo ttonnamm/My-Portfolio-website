a3ce1e636a87cbe2d01cd9ab354a5f91cc39d59a
ttonnamm <kanyarat.92305@gmail.com>
2024-12-24 23:12:36 +0700

    contact,footer&JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,32 @@ function linkAction(){
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+/*---------scroll-----------*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+/*scroll home*/
+sr.reveal('.home_title',{})
+sr.reveal('.button',{delay: 200})
+sr.reveal('.home_img',{delay: 400})
+sr.reveal('.home_social-icon',{interval: 200})
+
+/*scroll about*/
+sr.reveal('.about_img',{})
+sr.reveal('.about_subtitle',{delay: 200})
+sr.reveal('.about_text',{delay: 400})
+
+/*scroll skilss*/
+sr.reveal('.skills_text',{delay: 200})
+sr.reveal('.skills_data',{interval: 200})
+
+/*scroll project*/
+sr.reveal('.project_title',{interval: 200})
+sr.reveal('.project_subtitle',{interval: 200})
+sr.reveal('.project_img',{interval: 200})
+
+/*scroll contact*/
+sr.reveal('.contact_input',{interval: 200})
